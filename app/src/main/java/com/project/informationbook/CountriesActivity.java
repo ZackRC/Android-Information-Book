62b120ec81ce0e25810f5883d06f14124a666e63
zack <zackrcollett@gmail.com>
2023-02-24 08:33:12 +0200

    Add files

---
+++ app/src/main/java/com/project/informationbook/CountriesActivity.java
@@ -0,0 +1,14 @@
+package com.project.informationbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countries);
+    }
+}
